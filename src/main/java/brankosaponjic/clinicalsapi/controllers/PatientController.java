a77bbad87edec430227a735b09110204ea32b100
Branko <brankosaponjic76@gmail.com>
2021-02-26 12:51:51 +0100

    Implemented the GET and Save Patient methods. Closes #5

--- src/main/java/brankosaponjic/clinicalsapi/controllers/PatientController.java
+++ src/main/java/brankosaponjic/clinicalsapi/controllers/PatientController.java
@@ -3,9 +3,7 @@ package brankosaponjic.clinicalsapi.controllers;
 import brankosaponjic.clinicalsapi.model.Patient;
 import brankosaponjic.clinicalsapi.repositories.PatientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -23,5 +21,15 @@ public class PatientController {
     @RequestMapping(value = "/patients", method = RequestMethod.GET)
     public List<Patient> getPatients() {
         return patientRepository.findAll();
+    }
+
+    @RequestMapping(value = "/patients/{id}", method = RequestMethod.GET)
+    public Patient getPatient(@PathVariable("id") int id) {
+        return patientRepository.findById(id).get();
+    }
+
+    @RequestMapping(value = "/patients", method = RequestMethod.POST)
+    public Patient savePatient(@RequestBody Patient patient) {
+        return patientRepository.save(patient);
     }
 }

714c4cf628f4214f27767e2173149676bbed944a
Branko <brankosaponjic76@gmail.com>
2021-02-27 19:37:57 +0100

    Implemented Analyze Method and Calculate BMI. Closes #11

--- src/main/java/brankosaponjic/clinicalsapi/controllers/PatientController.java
+++ src/main/java/brankosaponjic/clinicalsapi/controllers/PatientController.java
@@ -1,10 +1,12 @@
 package brankosaponjic.clinicalsapi.controllers;
 
+import brankosaponjic.clinicalsapi.model.ClinicalData;
 import brankosaponjic.clinicalsapi.model.Patient;
 import brankosaponjic.clinicalsapi.repositories.PatientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -31,5 +33,26 @@ public class PatientController {
     @RequestMapping(value = "/patients", method = RequestMethod.POST)
     public Patient savePatient(@RequestBody Patient patient) {
         return patientRepository.save(patient);
+    }
+
+    @RequestMapping(value = "/patients/analyze/{id}", method = RequestMethod.GET)
+    public Patient analyze(@PathVariable("id") int id) {
+        Patient patient = patientRepository.findById(id).get();
+        List<ClinicalData> clinicalData = patient.getClinicalData();
+        ArrayList<ClinicalData> duplicateClinicalData = new ArrayList<>();
+        for (ClinicalData eachEntry : duplicateClinicalData) {
+            if (eachEntry.getComponentName().equals("hw")) {
+                String[] heightAndWeight = eachEntry.getComponentValue().split("/");
+                if (heightAndWeight != null && heightAndWeight.length>1) {
+                    float heightInMeters = Float.parseFloat(heightAndWeight[0])*0.4356F;
+                    float bmi = Float.parseFloat(heightAndWeight[1])/(heightInMeters*heightInMeters);
+                    ClinicalData bmiData = new ClinicalData();
+                    bmiData.setComponentName("bmi");
+                    bmiData.setComponentValue(Float.toString(bmi));
+                    clinicalData.add(bmiData);
+                }
+            }
+        }
+        return patient;
     }
 }
